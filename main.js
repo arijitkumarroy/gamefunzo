f2de7b5432784f70d7ae9fbb865788e5cf75cce2
arijitkumarroy <arijitkumarroy@yahoo.com>
2025-07-23 00:21:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+
+document.getElementById('game-area').innerHTML = '<iframe src="https://www.retrogames.cc/embed/35796-super-mario-bros.html" width="640" height="480" frameborder="no" allowfullscreen></iframe>';
